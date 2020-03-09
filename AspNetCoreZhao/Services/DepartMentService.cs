ad779b9931aac1e49d7c0cd9657099a017890670
zhao <zhao@ZHAO-PC>
2020-03-09 21:40:30 +0800

    services

---
+++ AspNetCoreZhao/Services/DepartMentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreZhao.Services
+{
+    public class DepartMentService: IDepartMentService
+    {
+        public DepartMentService()
+        {
+
+        }
+    }
+}
